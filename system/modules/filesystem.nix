2f3916cf300d05710889326b493a3a81fea258d6
jaximo <jaximo98dev@gmail.com>
2024-12-27 20:36:06 +0100

    NIx optimise

--- system/modules/filesystem.nix
+++ system/modules/filesystem.nix
@@ -3,6 +3,10 @@
 # 
 { config, pkgs, ... }:
 {
+
+  nix.optimise.automatic = true;
+  nix.optimise.dates = [ "03:45" ]; # Optional; allows customizing optimisation schedule
+
   environment.systemPackages = with pkgs; [
     inotify-tools
   ];
