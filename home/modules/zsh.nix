a39a434c5085cb298ce0fa0e3402a6b2d6d20296
jaximo_dev <jaximo98dev@gmail.com>
2025-01-14 20:53:00 +0100

    zsh theme

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -17,6 +17,21 @@ in
     
     initExtra = ''
 
+      zstyle :prompt:pure:path color "#83a598"
+      zstyle :prompt:pure:git:branch color "#fe8019"
+      zstyle :prompt:pure:git:dirty color "#fb4934"
+      zstyle :prompt:pure:prompt:success color "#b8bb26"
+      zstyle :prompt:pure:prompt:continuation color "#fe8019"
+      zstyle :prompt:pure:prompt:error color "#fb4934"
+      zstyle :prompt:pure:git:arrow color "#8ec07c"
+      zstyle :prompt:pure:execution_time color "#fabd2f"
+      zstyle :prompt:pure:git:stash color "#8ec07c"
+      zstyle :prompt:pure:git:action color "#fabd2f"
+      zstyle :prompt:pure:host color "#fe8019"
+      zstyle :prompt:pure:user color "#fe8019"
+      zstyle :prompt:pure:virtualenv color "#fabd2f"
+
+
       if [ -n "''${commands[fzf-share]}" ]; then
         source "''$(fzf-share)/key-bindings.zsh"
         source "''$(fzf-share)/completion.zsh"
